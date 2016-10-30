048a207e13ca40dc7152510632525e72f5fa2453
zitek1997 <zitek1997@o2.pl>
2016-10-30 19:36:07 +0100

    modyfikacja config.php i dodanie index.php

--- ajax/config.php
+++ ajax/config.php
@@ -1,14 +1,7 @@
 <?php
 session_start();
 require_once('Database.php');
-if (isset($_SESSION['uid']))
-{
-  $id=$_SESSION['uid'];
-  $dbb="db".$dbb.".php";
-  require_once("$dbb");
-}else{
-  require_once("db.php");
-}
+require_once("db.php");
 $db = new Database();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
--- config.php
+++ config.php
@@ -1,14 +1,7 @@
 <?php
 session_start();
 require_once('ajax/Database.php');
-if (isset($_SESSION['uid']))
-{
-  $id=$_SESSION['uid'];
-  $dbb="ajax/db".$dbb.".php";
-  require_once("$dbb");
-}else{
-  require_once('ajax/db.php');
-}
+require_once('ajax/db.php');
 $db = new Database();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
