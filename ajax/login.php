62495102ebc63b85341b1adf44e3fc238c7ead60
olo36 <olo36@DESKTOP-UUGHES0>
2016-10-30 13:02:40 +0100

    Login Ajax z walidacją Angular

--- ajax/login.php
+++ ajax/login.php
@@ -2,6 +2,12 @@
 include("config.php");
 // $login=$_POST['login'];
 // $pass=$_POST['pass'];
+
+$data = json_decode(file_get_contents("php://input"));
+$con = mysqli_connect('sql.itcave.nazwa.pl','itcave_2','Perseusz20^!','itcave_2');
+$login = mysqli_real_escape_string($con, $data->login);
+$pass = mysqli_real_escape_string($con, $data->pass);
+
 $array=array("LOGIN"=>$login);
 $sql="SELECT * FROM users WHERE LOGIN = :LOGIN";
 $usr=$db->select($sql, $array);
@@ -18,11 +24,19 @@ if($x==1)
   {
     $_SESSION['uid']=$usr['ID'];
     $_SESSION['login']=$usr['LOGIN'];
-    $_SESSION['imie']=$usr['IMIE'];
-    $_SESSION['nazwisko']=$usr['NAZWISKO'];
-    $_SESSION['idp']=$usr['ID_P'];
+    $_SESSION['imie']=$usr['FNAME'];
+    $_SESSION['nazwisko']=$usr['LNAME'];
+    $_SESSION['email']=$usr['EMAIL'];
+    $_SESSION['phone']=$usr['PHONE'];
   }
 }else{
   $_SESSION['logerr']=1;
 }
+
+print_r($_SESSION['uid']);
+print_r($_SESSION['login']);
+print_r($_SESSION['imie']);
+print_r($_SESSION['nazwisko']);
+print_r($_SESSION['email']);
+print_r($_SESSION['phone']);
 ?>
