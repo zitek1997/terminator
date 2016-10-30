067f4c319f1d758aea1d8deccba78893f2249bf7
zitek1997 <zitek1997@o2.pl>
2016-10-30 13:52:46 +0100

    oddzielne pliki definiujące bazę dla każdej firmy

--- ajax/config.php
+++ ajax/config.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
 require_once('Database.php');
-require_once('db.php');
+if (isset($_SESSION['firma_id']))
+{
+  $id=$_SESSION['firma_id'];
+  $dbb="db".$dbb.".php";
+  require_once("$dbb");
+}else{
+  require_once("db.php");
+}
 $db = new Database();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
 require_once('ajax/Database.php');
-require_once('ajax/db.php');
+if (isset($_SESSION['firma_id']))
+{
+  $id=$_SESSION['firma_id'];
+  $dbb="ajax/db".$dbb.".php";
+  require_once("$dbb");
+}else{
+  require_once('ajax/db.php');
+}
 $db = new Database();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
