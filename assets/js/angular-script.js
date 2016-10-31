2dc77c588a10e738fd6011ef0fee321b6830af0f
olo36 <olo36@DESKTOP-UUGHES0>
2016-10-31 21:39:57 +0100

    Dodawanie profili klientów, aktualizacja linków -.-

--- assets/js/angular-script.js
+++ assets/js/angular-script.js
@@ -2,14 +2,22 @@ var crudApp = angular.module('crudApp',[]);
 crudApp.controller("DbController",['$scope','$http', function($scope,$http){
 
 $scope.getInfo = function(info){
-$http.post('ajax/login.php',{"login":info.login,"pass":info.pass}).success(function(data){
-$scope.details = data;
+$http.post('/ajax/login.php',{"login":info.login,"pass":info.pass}).success(function(data){
+window.location.replace("http://projekt.itcave.pl");
 });
 }
 
 
 $scope.insertInfo = function(info){
-$http.post('ajax/register.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone}).success(function(data){
+$http.post('/ajax/register.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone}).success(function(data){
+if (data == true) {
+$('#empForm').css('display', 'none');
+}
+});
+}
+
+$scope.addClient = function(info){
+$http.post('/ajax/addclient.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone}).success(function(data){
 if (data == true) {
 $('#empForm').css('display', 'none');
 }
