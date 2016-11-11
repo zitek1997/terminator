af8c1570afef88a32a7157dca73c518a26f71dd0
zitek1997 <zitek1997@o2.pl>
2016-11-11 12:47:48 +0100

    usunięcie kosza

--- assets/js/terminator.js
+++ assets/js/terminator.js
@@ -17,20 +17,20 @@ function getFreshEvents(){
 }
 
 function isElemOverDiv() {
-			var trashEl = jQuery('#trash');
-
-			var ofs = trashEl.offset();
-
-			var x1 = ofs.left;
-			var x2 = ofs.left + trashEl.outerWidth(true);
-			var y1 = ofs.top;
-			var y2 = ofs.top + trashEl.outerHeight(true);
-
-			if (currentMousePos.x >= x1 && currentMousePos.x <= x2 &&
-					currentMousePos.y >= y1 && currentMousePos.y <= y2) {
-
-					return true;
-			}
+			// var trashEl = jQuery('#trash');
+			//
+			// // var ofs = trashEl.offset();
+			//
+			// var x1 = ofs.left;
+			// // var x2 = ofs.left + trashEl.outerWidth(true);
+			// var y1 = ofs.top;
+			// // var y2 = ofs.top + trashEl.outerHeight(true);
+			//
+			// if (currentMousePos.x >= x1 && currentMousePos.x <= x2 &&
+			// 		currentMousePos.y >= y1 && currentMousePos.y <= y2) {
+			//
+			// 		return true;
+			// }
 			return false;
 	}
 
