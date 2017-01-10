dfff3087d5d0a902516bfba6fdb76b0d6b85c85d
olo36 <olo36@192.168.1.65>
2017-01-10 16:45:25 +0100

    reszta

--- assets/js/angular-script.js
+++ assets/js/angular-script.js
@@ -18,7 +18,7 @@ $http.post('/ajax/login.php',{"login":info.login,"pass":info.pass})
 
 
 $scope.insertInfo = function(info){
-$http.post('/ajax/register.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone}).success(function(data){
+$http.post('/ajax/register.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone,"access":info.access}).success(function(data){
 if (data == true) {
 $('#empForm').css('display', 'none');
 }
@@ -29,6 +29,14 @@ $scope.addClient = function(info){
 $http.post('/ajax/addclient.php',{"fname":info.fname,"lname":info.lname,"login":info.login,"pass":info.pass,"email":info.email,"phone":info.phone}).success(function(data){
 if (data == true) {
 $('#empForm').css('display', 'none');
+}
+});
+}
+
+$scope.addTerm = function(info){
+$http.post('/ajax/addclient.php',{"name":info.name,"color":info.lname,"access":info.access}).success(function(data){
+if (data == true) {
+$('#termForm').css('display', 'none');
 }
 });
 }
